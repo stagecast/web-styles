fd3f568c73b682c24557e3b6c8286fde7248874a
Filippo Boiani <filippo.boiani2@gmail.com>
2019-01-18 15:59:53 +0100

    icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,37 @@
-var gulp  = require('gulp'),
+var gulp = require('gulp'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   cleanCss = require('gulp-clean-css'),
   rename = require('gulp-rename'),
-  postcss      = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  svgSprite = require("gulp-svg-sprites");
 
-gulp.task('build-theme', function() {
-  return gulp.src(['scss/*.scss'])
+gulp.task('sprites', function () {
+  return gulp
+    .src('scss/svg/*.svg')
+    .pipe(svgSprite())
+    .pipe(gulp.dest("css/imgs/icons/"));
+});
+
+gulp.task('build-theme', function () {
+  return gulp
+    .src(['scss/*.scss'])
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([ autoprefixer({ browsers: [
-      'Chrome >= 35',
-      'Firefox >= 38',
-      'Edge >= 12',
-      'Explorer >= 10',
-      'iOS >= 8',
-      'Safari >= 8',
-      'Android 2.3',
-      'Android >= 4',
-      'Opera >= 12']})]))
+    .pipe(postcss([autoprefixer({
+        browsers: [
+          'Chrome >= 35',
+          'Firefox >= 38',
+          'Edge >= 12',
+          'Explorer >= 10',
+          'iOS >= 8',
+          'Safari >= 8',
+          'Android 2.3',
+          'Android >= 4',
+          'Opera >= 12'
+        ]
+      })]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css/'))
     .pipe(cleanCss())
@@ -27,12 +39,10 @@ gulp.task('build-theme', function() {
     .pipe(gulp.dest('css/'))
 });
 
-gulp.task('watch', ['build-theme'], function() {
-  gulp.watch(['scss/*.scss', 'scss/*/*.scss'], ['build-theme']);
+gulp.task('watch', ['build-theme'], function () {
+  gulp.watch([
+    'scss/*.scss', 'scss/*/*.scss'
+  ], ['build-theme']);
 });
 
-gulp.task('default', ['build-theme'], function() {
-});
-
-
-
+gulp.task('default', ['build-theme'], function () {});
